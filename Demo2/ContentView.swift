cec61d3289d2657088bac44f9e9fc6e97292b474
Manikantha-96 <57246633+Manikantha-96@users.noreply.github.com>
2022-08-07 09:22:15 +0530

    Add files via upload

---
+++ Demo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo2
+//
+//  Created by Venkata Phanindhra on 07/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
